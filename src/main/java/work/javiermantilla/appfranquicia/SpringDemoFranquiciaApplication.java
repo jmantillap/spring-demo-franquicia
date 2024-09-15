a4d8d57741521651c8a7549a085846ec37da1e5e
jmantillap <jmantillap@gmail.com>
2024-09-15 13:58:37 -0500

    Se realiza commit inicial

---
+++ src/main/java/work/javiermantilla/appfranquicia/SpringDemoFranquiciaApplication.java
@@ -0,0 +1,13 @@
+package work.javiermantilla.appfranquicia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoFranquiciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoFranquiciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/work/javiermantilla/appfranquicia/SpringDemoFranquiciaApplicationTests.java
@@ -0,0 +1,13 @@
+package work.javiermantilla.appfranquicia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoFranquiciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
